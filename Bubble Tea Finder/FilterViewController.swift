2065c9e98df8623ec73dfd11b81f4e867760aae4
ElijahButers <Elijahbuters@gmail.com>
2016-09-24 17:00:01 +0300

    Added code to saveButtonTapped() in FilterViewController where every time we tap Search in the top-right corner of the filter/sort screen, we’ll notify the delegate of our selection and dismiss the filter/sort screen to reveal the list of venues behind it.

--- Bubble Tea Finder/FilterViewController.swift
+++ Bubble Tea Finder/FilterViewController.swift
@@ -145,6 +145,8 @@ class FilterViewController: UITableViewController {
   
   @IBAction func saveButtonTapped(sender: UIBarButtonItem) {
     
+    delegate?.filterViewController(self, didSelectPredicate: selectedPredicate, sortDescriptor: selectedSortDescriptor)
+    
     dismissViewControllerAnimated(true, completion:nil)
   }
 }
