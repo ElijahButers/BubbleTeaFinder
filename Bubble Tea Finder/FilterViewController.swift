e47d9f29f832789dca545a392ed96ecdc1a6b8be
ElijahButers <Elijahbuters@gmail.com>
2016-09-25 14:12:02 +0300

    Added a lazy property to FilterViewController that sorts by distance in ascending order.

--- Bubble Tea Finder/FilterViewController.swift
+++ Bubble Tea Finder/FilterViewController.swift
@@ -84,6 +84,11 @@ class FilterViewController: UITableViewController {
         var sd = NSSortDescriptor(key: "name", ascending: true, selector: #selector(NSString.localizedStandardCompare(_:)))
         return sd
     }()
+    
+    lazy var distanceSortDescriptor: NSSortDescriptor = {
+        var sd = NSSortDescriptor(key: "location.distance", ascending: true)
+        return sd
+    }()
   
     func populateCheapVenueCountLabel() {
         
