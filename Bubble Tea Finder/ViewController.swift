107ffda0a50d89118c0f9c8c7d44eec44497c1d8
ElijahButers <Elijahbuters@gmail.com>
2016-09-26 13:18:33 +0300

    Added code to viewDidLoad() in ViewController that sets up a batch update request to mark every Venue in Core Data as our favorite.

--- Bubble Tea Finder/ViewController.swift
+++ Bubble Tea Finder/ViewController.swift
@@ -23,6 +23,19 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    let batchUpdate = NSBatchUpdateRequest(entityName: "Venue")
+    
+    batchUpdate.propertiesToUpdate = ["favorite" : NSNumber(bool: true)]
+    batchUpdate.affectedStores = coreDataStack.context.persistentStoreCoordinator!.persistentStores
+    batchUpdate.resultType = .UpdatedObjectsCountResultType
+    
+    do {
+        let batchResult = try coreDataStack.context.executeRequest(batchUpdate) as! NSBatchUpdateResult
+        print("Records updated \(batchResult.result!)")
+    } catch let error as NSError {
+        print("Could not update \(error), \(error.userInfo)")
+    }
+    
     fetchRequest = NSFetchRequest(entityName: "Venue")
     
     asyncFetchRequest = NSAsynchronousFetchRequest(fetchRequest: fetchRequest) { [unowned self] (result: NSAsynchronousFetchResult) -> Void in
